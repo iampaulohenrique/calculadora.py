6f7ff942d2acedabc1a743d11a9a442fb8531b37
Paulo Henrique <contatoiampaulohenrique@gmail.com>
2024-08-20 15:02:58 -0300

    primeiro commit

---
+++ Calculadora.py
@@ -0,0 +1,14 @@
+#SOMA
+
+n1 = int(input('digite o primeiro número:'))
+n2 = int(input('digite o segundo número:'))
+
+soma = n1 + n2
+subtracao = n1 - n2
+multiplicacao = n1 * n2
+divisao = n1 / n2
+
+print('A soma entre {} e {} é: {}'.format(n1 ,n2 ,soma))
+print('A subtração entre {} e {} e: {}'. format(n1 ,n2 ,subtracao))
+print('A multiplicação entre {} e {} é: {}'.format(n1 ,n2 ,multiplicacao))
+print('A divisão entre {} e {} é:{}'.format(n1 ,n2 ,divisao))
